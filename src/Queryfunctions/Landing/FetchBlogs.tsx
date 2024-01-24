186a705e8fbf4c8e3f75b71751ac46f997a1edd1
Muaaz <yourssharky@gmail.com>
2024-01-24 19:08:51 +0500

    update FetchBlogs.tsx

--- src/Queryfunctions/Landing/FetchBlogs.tsx
+++ src/Queryfunctions/Landing/FetchBlogs.tsx
@@ -9,13 +9,11 @@ return response
 }
 
 export const FetchStarter = async(count:number) => {
-    // let data = useAppSelector(state=>state.landing)
-    // let dispatch = useAppDispatch()
-    // let {count} = data
+
 
 let interests=null
     let response = await Axios.post(`/posts/starter`,{count,interests})
     return response.data
-    // dispatch(insertion({count:count+1,Blogs:response.data?.payload?.Blogs,Topics:response.data?.payload?.Topics,Trendings:response.data?.payload?.Trendings}))
+
     }
 export default FetchBlogs
