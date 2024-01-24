52ff3cfd487f815bac17db278845f2bddb34bdce
Muaaz <yourssharky@gmail.com>
2024-01-24 18:45:22 +0500

    update ReadComments.ts

--- src/Queryfunctions/Comment/ReadComments.ts
+++ src/Queryfunctions/Comment/ReadComments.ts
@@ -7,7 +7,7 @@ export interface Iapi<T>{
 }
 
 const ReadComments = async(count:number=0,postId:string) => {
-      let response = await Axios.post<Iapi<IcommentState> >(`/comments/${postId}`,{},{headers:{"count":count}});
+      let response = await Axios.post<Iapi<IcommentState> >(`/comments/read/${postId}`,{},{headers:{"count":count}});
       
       return response.data;
     
@@ -16,7 +16,7 @@ const ReadComments = async(count:number=0,postId:string) => {
 
 
 export const ReadCommentsFn = async<T>(data:IcommentState,dispatch:T,postId:string) => {
-    let response = await Axios.post<Iapi<IcommentState>>(`/comments/${postId}`,{},{headers:{"count":data.count}})
+    let response = await Axios.post<Iapi<IcommentState>>(`/comments/read/${postId}`,{},{headers:{"count":data.count}})
     let payload = response.data.payload
     if (typeof dispatch =="function") {
         dispatch(CommentInsertion({count:payload.count,Comment:[...data.Comment, ...payload.Comment],totalResults:payload.totalResults}))
